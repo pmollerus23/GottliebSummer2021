9471b36c604359a25b563aa3b4d9e4e9319113f8
cbell0 <85899001+cbell0@users.noreply.github.com>
2021-07-09 14:51:39 -0400

    abtPlots and pcPlots

--- Data/pcFR.m
+++ Data/pcFR.m
@@ -18,44 +18,74 @@ x2 = [130 140 150 160 170 180 190 200 210 220 230 240];
 
 
 for i = 1:2 
-    vals = [];
-   idx =  find(ff == i & sn == pCode & FR > 0)
-   vals = FR(idx);
-   
+    vals1 = [];
+    vals2 = [];
+    
+    
+   idx =  find(ff == i & sn == pCode & FR > 0 & TrSp == 1)
+   vals1 = FR(idx);
+   idx2 = find(ff == i & sn == pCode & FR > 0 & TrSp == 2);
+   vals2 = FR(idx2);
+   totLength = length(vals1) + length(vals2);
    if i == 1
-      for j = 1:length(vals)
+      for j = 1:length(vals1)
           if j == 1
-          x1 = zeros(1,(length(vals)-1));
+          x1 = zeros(1,(length(vals1)-1));
           f = [1];
           horzcat(f,x1);
           elseif j>1
           x1(j) = x1(j-1) + 10;
           end
-          finalIdx = length(vals)
+          finalIdx = length(vals1)
 
       end 
-      plot(x1, vals, 'bo-'); hold on 
+      for j = 1:length(vals2)
+          if j == 1
+          x2 = zeros(1,(length(vals2)-1));
+          f = [x1(length(vals1)) + 10];
+         x2 =  horzcat(f,x2);
+          elseif j > 1
+               x2(j) = x2(j-1) + 10;
+          end
+          finalVal = x2(j);
+      end
+      plot(x1, vals1, 'bo-'); hold on 
+      plot(x2, vals2, 'bx-'); hold on 
+
       
       
       
    elseif i == 2
-       for j = 2:length(vals)
+       for j = 1:length(vals1)
            if j ==1
-               x2 = [(x1(finalIdx)+10)];
-               xToCat = zeros(1, length(vals) - 1)
-               x2 = horzcat(x2,xToCat);
+               x3(1) = [(finalVal+10)];
+               xToCat = zeros(1, length(vals1) - 1)
+               x3 = horzcat(x3,xToCat);
            elseif j > 1
-               x2(j) = x2(j-1) + 10;
+               x3(j) = x3(j-1) + 10;
            end
+           finalVal = x3(j);
        end
-      plot(x2, vals, 'rx-'); hold on
+       for j = 1:length(vals2)
+          if j == 1
+          x4 = zeros(1,(length(vals2)-1));
+          f = [finalVal];
+          x4 = horzcat(f,x4);
+          elseif j > 1
+               x4(j) = x4(j-1) + 10;
+          end
+      end
+      
+      plot(x3, vals1, 'ro-'); hold on
+      plot(x4, vals2, 'rx-'); hold on
+
     
     
     
    end 
 
    
-   legend('Free Choice','Force Choice')
+   legend('Regular Free Choice','Speeded Free Choice', 'Regular Force Choice', 'Speeded Force Choice');
  title('Fatigue Ratings By Trials')
     xlabel('Trials Number (1-240)')
     ylabel('Fatigue Rating (1-10)');
