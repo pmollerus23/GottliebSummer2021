5b71b3c4b5d2c45f52c49486ffe6cefd7ccabfb4
mollerup23 <pmollerus23@gmail.com>
2021-07-23 12:30:06 -0400

    Added percent correct to PPS

--- Data/plotPerSubject.m
+++ Data/plotPerSubject.m
@@ -55,13 +55,40 @@ for i = 1:length(SC)
              polarPlotsPC(d, SC(i));
 
     end
+    block = d(:,17);
+    speed = d(:,9);
+    accs = d(:,5);
+    conf = d(:,12);
     
+    slwCrrct = 0;
+    spCrrct = 0;
+    for i = 1:length(d)
+        if speed(i) == 1 && accs(i) == 1
+            slwCrrct = slwCrrct + 1;
+        elseif speed(i) == 2 && accs(i) == 1
+            spCrrct = spCrrct + 1;
+        end
+    end
     
+    slowTotal = length(find(speed == 1));
+    fastTotal = length(find(speed == 2));
+    
+    slwPct = (slwCrrct / slowTotal) * 100;
+    spPct = (spCrrct / fastTotal) * 100;
+    slwRate = conf(length(find(speed == 1)));
+    spRate = conf(length(find(speed == 2)));
+    
+    STRslwPctEst = num2str(slwRate);
+    STRslwPctAct = num2str(slwPct);
+    
+    STRspPctEst = num2str(spRate);
+    STRspPctAct = num2str(spPct); 
     
     titleHead = 'Participant';
     SCstr = num2str(SC(i));
-    titleStr = strcat(titleHead, {' '}, SCstr, {' '}, {'(Regular/Speeded = Blue/Red, No Feedback/Feedback = O/X)'}, {' '}, );
+    titleStr = strcat(titleHead, {' '}, SCstr, {' '}, {'(Regular/Speeded = Blue/Red, No Feedback/Feedback = O/X)'}, {' '}, 'slw % est: ', STRslwPctEst,{' '}, 'slw % act: ', STRslwPctAct, 'sp % est: ', {' '}, STRspPctEst, {' '}, 'sp % act: ', STRspPctAct);
     suptitle(titleStr);
+    
                %  polarPlotsRT(d, SC(i));
                     %          polarPlotsPC(d, SC(i));
 
